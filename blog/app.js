66d2f5d13734bd7889e018a1f08ccda293eab490
CROSS <CROSS>
2015-12-16 14:19:11 +0800

    updata 12/16

--- blog/app.js
+++ blog/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -18,12 +18,15 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+routes(app);
+
+app.listen(app.get('port'),function(){
+  console.log('Express server listening on port '+app.get('port'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
